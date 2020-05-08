dcea77a1e9ae079d86d908c413274631d6e6ecac
Dawn Ho <dawn.ho@gmail.com>
2020-05-07 22:26:31 -0700

    comment out adafruit

--- spi/src/sensor.py
+++ spi/src/sensor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import Adafruit_BBIO.SPI as SPI
+# import Adafruit_BBIO.SPI as SPI
 import time
 
-spi_zero = SPI(1,0)
-spi_one = SPI(1,1)
+# spi_zero = SPI(1,0)
+# spi_one = SPI(1,1)
 
 while(True):
     time.sleep(0.5)
